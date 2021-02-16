ddace981342f6dc02836aac0866573d9baad8669
Anneury <Anneury@DESKTOP-AAVNCBB>
2021-02-15 20:13:31 -0400

    Add project files.

---
+++ rPermisos.Designer.cs
@@ -0,0 +1,175 @@
+﻿
+namespace Registro_Roles
+{
+    partial class rPermisos
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(rPermisos));
+            this.IdnumericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_descripcion = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_guardar = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.btn_eliminar = new System.Windows.Forms.Button();
+            this.btn_nuevo = new System.Windows.Forms.Button();
+            this.btn_buscar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.IdnumericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // IdnumericUpDown1
+            // 
+            this.IdnumericUpDown1.Location = new System.Drawing.Point(33, 44);
+            this.IdnumericUpDown1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.IdnumericUpDown1.Name = "IdnumericUpDown1";
+            this.IdnumericUpDown1.Size = new System.Drawing.Size(87, 27);
+            this.IdnumericUpDown1.TabIndex = 37;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 20);
+            this.label3.TabIndex = 36;
+            this.label3.Text = "RolId";
+            // 
+            // txt_descripcion
+            // 
+            this.txt_descripcion.Location = new System.Drawing.Point(33, 136);
+            this.txt_descripcion.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txt_descripcion.Name = "txt_descripcion";
+            this.txt_descripcion.Size = new System.Drawing.Size(262, 27);
+            this.txt_descripcion.TabIndex = 29;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 20);
+            this.label1.TabIndex = 27;
+            this.label1.Text = "Descripcion";
+            // 
+            // btn_guardar
+            // 
+            this.btn_guardar.Image = ((System.Drawing.Image)(resources.GetObject("btn_guardar.Image")));
+            this.btn_guardar.Location = new System.Drawing.Point(154, 192);
+            this.btn_guardar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btn_guardar.Name = "btn_guardar";
+            this.btn_guardar.Size = new System.Drawing.Size(91, 37);
+            this.btn_guardar.TabIndex = 38;
+            this.btn_guardar.Text = "Guardar";
+            this.btn_guardar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btn_guardar.UseVisualStyleBackColor = true;
+            this.btn_guardar.Click += new System.EventHandler(this.btn_guardar_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // btn_eliminar
+            // 
+            this.btn_eliminar.Image = ((System.Drawing.Image)(resources.GetObject("btn_eliminar.Image")));
+            this.btn_eliminar.Location = new System.Drawing.Point(280, 192);
+            this.btn_eliminar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btn_eliminar.Name = "btn_eliminar";
+            this.btn_eliminar.Size = new System.Drawing.Size(91, 37);
+            this.btn_eliminar.TabIndex = 42;
+            this.btn_eliminar.Text = "Eliminar";
+            this.btn_eliminar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btn_eliminar.UseVisualStyleBackColor = true;
+            this.btn_eliminar.Click += new System.EventHandler(this.btn_eliminar_Click);
+            // 
+            // btn_nuevo
+            // 
+            this.btn_nuevo.Image = ((System.Drawing.Image)(resources.GetObject("btn_nuevo.Image")));
+            this.btn_nuevo.Location = new System.Drawing.Point(29, 192);
+            this.btn_nuevo.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btn_nuevo.Name = "btn_nuevo";
+            this.btn_nuevo.Size = new System.Drawing.Size(91, 37);
+            this.btn_nuevo.TabIndex = 44;
+            this.btn_nuevo.Text = "Nuevo";
+            this.btn_nuevo.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btn_nuevo.UseVisualStyleBackColor = true;
+            this.btn_nuevo.Click += new System.EventHandler(this.btn_nuevo_Click);
+            // 
+            // btn_buscar
+            // 
+            this.btn_buscar.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_buscar.Image = ((System.Drawing.Image)(resources.GetObject("btn_buscar.Image")));
+            this.btn_buscar.Location = new System.Drawing.Point(132, 44);
+            this.btn_buscar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btn_buscar.Name = "btn_buscar";
+            this.btn_buscar.Size = new System.Drawing.Size(75, 27);
+            this.btn_buscar.TabIndex = 45;
+            this.btn_buscar.Text = "Buscar";
+            this.btn_buscar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btn_buscar.UseVisualStyleBackColor = true;
+            this.btn_buscar.Click += new System.EventHandler(this.btn_buscar_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(399, 259);
+            this.Controls.Add(this.btn_buscar);
+            this.Controls.Add(this.btn_nuevo);
+            this.Controls.Add(this.btn_eliminar);
+            this.Controls.Add(this.btn_guardar);
+            this.Controls.Add(this.IdnumericUpDown1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_descripcion);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Registro de Permisos";
+            ((System.ComponentModel.ISupportInitialize)(this.IdnumericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown IdnumericUpDown1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_descripcion;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_guardar;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.Button btn_nuevo;
+        private System.Windows.Forms.Button btn_eliminar;
+        private System.Windows.Forms.Button btn_buscar;
+    }
+}
+
---
+++ rPermisos.cs
@@ -0,0 +1,139 @@
+﻿using Registro_Roles.BLL;
+using Registro_Roles.DAL;
+using Registro_Roles.Entidades;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Registro_Roles
+{
+    public partial class rPermisos : Form
+    {
+        public rPermisos()
+        {
+            InitializeComponent();
+        }
+
+        private void Limpiar()
+        {
+            IdnumericUpDown1.Value = 0;
+            txt_descripcion.Text = string.Empty;
+            errorProvider1.Clear();
+        }
+
+        private void LlenaCampo(Roles rol)
+        {
+            IdnumericUpDown1.Value = rol.RolId;
+            txt_descripcion.Text = rol.Descripcion;
+        }
+
+        private Roles LlenaClase()
+        {
+            Roles rol = new Roles();
+            rol.RolId = Convert.ToInt32(IdnumericUpDown1.Value);
+            rol.Descripcion = txt_descripcion.Text;
+
+            return rol;
+        }
+        private bool ExisteEnLaBaseDeDatos()
+        {
+            Roles rol = RolesBLL.Buscar((int)IdnumericUpDown1.Value);
+
+            return (rol != null);
+        }
+
+        private void btn_guardar_Click(object sender, EventArgs e)
+        {
+            Roles rol;
+            bool paso = false;
+
+            if (!Validar())
+                return;
+
+            rol = LlenaClase();
+
+            if (IdnumericUpDown1.Value == 0)
+                paso = RolesBLL.Guardar(rol);
+            else
+            {
+                if (!ExisteEnLaBaseDeDatos())
+                {
+                    MessageBox.Show("No se puede modificar", "Fallo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                paso = RolesBLL.Modificar(rol);
+            }
+
+            if (paso)
+            {
+                Limpiar();
+                MessageBox.Show("Guardado!!", "Correcto", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("No fue posible guardar!!", "Fallo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private bool Validar()
+        {
+            bool paso = true;
+            errorProvider1.Clear();
+            if(txt_descripcion.Text == string.Empty)
+            {
+                errorProvider1.SetError(txt_descripcion, "El campo descripcion no puede estar vacio");
+                txt_descripcion.Focus();
+                paso = false;
+            }
+            return paso;
+        }
+
+        private void btn_nuevo_Click(object sender, EventArgs e)
+        {
+            Limpiar();
+        }
+
+        private void btn_eliminar_Click(object sender, EventArgs e)
+        {
+            errorProvider1.Clear();
+
+            int id;
+            int.TryParse(IdnumericUpDown1.Text, out id);
+
+            Limpiar();
+
+            if (RolesBLL.Eliminar(id))
+                MessageBox.Show("Eliminado", "Exito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else
+                errorProvider1.SetError(IdnumericUpDown1, "Rol no existe");
+        }
+
+        private void btn_buscar_Click_1(object sender, EventArgs e)
+        {
+            int id;
+            Roles rol = new Roles();
+            int.TryParse(IdnumericUpDown1.Text, out id);
+
+            Limpiar();
+
+            rol = RolesBLL.Buscar(id);
+
+            if(rol != null)
+            {
+                MessageBox.Show("Rol encontrado");
+                LlenaCampo(rol);
+            }
+            else
+            {
+                MessageBox.Show("Rol no encontrado");
+            }
+        }
+
+    }
+}
